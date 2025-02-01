268d03edb8113ae47775b8f61a3b0eb6f7230aa0
oleksandrafriz <ipz221_foi@student.ztu.edu.ua>
2025-02-01 22:25:56 +0200

    added some info about films and page for every film from DB

---
+++ MySite/Models/MovieGenre.cs
@@ -0,0 +1,13 @@
+﻿namespace MySite.Models
+{
+    public class MovieGenre
+    {
+        public int Id { get; set; } // Окремий ключ
+
+        public int movie_id { get; set; }
+        public Movie Movie { get; set; }
+
+        public int genre_id { get; set; }
+        public Genre Genre { get; set; }
+    }
+}
