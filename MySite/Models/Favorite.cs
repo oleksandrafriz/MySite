7c4f5548bb1b03d251250e84f60160e92e53f536
oleksandrafriz <ipz221_foi@student.ztu.edu.ua>
2025-02-03 22:00:34 +0200

    added favorite films page

---
+++ MySite/Models/Favorite.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MySite.Models
+{
+    public class Favorite
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [ForeignKey("User")]
+        public int user_id { get; set; }
+
+        [ForeignKey("Movie")]
+        public int movie_id { get; set; }
+
+        public User User { get; set; }  // Навігаційна властивість
+        public Movie Movie { get; set; } // Навігаційна властивість
+    }
+}
